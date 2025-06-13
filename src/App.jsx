7bbaae1b9015bcb6e624fe83492b5b421bc64055
SyedSybtain <sybtainsyed@gmail.com>
2025-06-13 09:12:23 +0500

    style updates

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [show,setShow] = useState(false)
 
   return (
-    <div className='bg-[url(https://i.pinimg.com/originals/8e/46/15/8e46150f790fbefe438d9c2767c32ad1.gif)] min-w-screen min-h-screen text-white font-mono p-8'>
+    <div className='bg-[url(https://i.pinimg.com/originals/8e/46/15/8e46150f790fbefe438d9c2767c32ad1.gif)] min-w-screen sm:w-[300px] min-h-screen text-white font-mono p-8'>
       <div className=' min-w-screen min-h-screen bg-black bg-opacity-70 p-8 rounded-2xl flex flex-col border-2'>
 
         <div className='w-[30%] h-max bg-sky-700 bg-opacity-90 p-4 rounded-xl flex flex-col items-center justify-center border-4 self-center'>
